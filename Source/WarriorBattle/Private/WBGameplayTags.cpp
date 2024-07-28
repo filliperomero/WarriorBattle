d8d2ddc5fe07bf7875af3e284afb96e6ec9d4b77
filliperomero <fillipe.romero@gmail.com>
2024-07-27 19:06:24 -0600

    Add Input Tags (Gameplay Tags)

---
+++ Source/WarriorBattle/Private/WBGameplayTags.cpp
@@ -0,0 +1,10 @@
+﻿// Fillipe Romero - All Rights Reserved
+
+#include "WBGameplayTags.h"
+
+namespace WBGameplayTags
+{
+	/** Input Tags */
+	UE_DEFINE_GAMEPLAY_TAG(InputTag_Move, "InputTag.Move")
+	UE_DEFINE_GAMEPLAY_TAG(InputTag_Look, "InputTag.Look")
+}
---
+++ Source/WarriorBattle/Public/WBGameplayTags.h
@@ -0,0 +1,12 @@
+﻿// Fillipe Romero - All Rights Reserved
+
+#pragma once
+
+#include "NativeGameplayTags.h"
+
+namespace WBGameplayTags
+{
+	/** Input Tags */
+	WARRIORBATTLE_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(InputTag_Move)
+	WARRIORBATTLE_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(InputTag_Look)
+}
