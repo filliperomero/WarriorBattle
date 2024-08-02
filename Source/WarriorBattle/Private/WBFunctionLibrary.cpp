064462dd9ad33244adb78ffd9727664dcec6ad76
filliperomero <fillipe.romero@gmail.com>
2024-08-02 17:05:32 -0600

    Add a way to jump to the finisher attack (from Heavy attack) from light attack (3rd attack enables it)

---
+++ Source/WarriorBattle/Private/WBFunctionLibrary.cpp
@@ -0,0 +1,45 @@
+﻿// Fillipe Romero - All Rights Reserved
+
+#include "WBFunctionLibrary.h"
+
+#include "AbilitySystemBlueprintLibrary.h"
+#include "AbilitySystem/WBAbilitySystemComponent.h"
+
+UWBAbilitySystemComponent* UWBFunctionLibrary::NativeGetWBASCFromActor(AActor* InActor)
+{
+	check(InActor);
+	
+	return CastChecked<UWBAbilitySystemComponent>(UAbilitySystemBlueprintLibrary::GetAbilitySystemComponent(InActor));
+}
+
+bool UWBFunctionLibrary::NativeDoesActorHaveTag(AActor* InActor, FGameplayTag TagToCheck)
+{
+	UWBAbilitySystemComponent* ASC = NativeGetWBASCFromActor(InActor);
+
+	return ASC->HasMatchingGameplayTag(TagToCheck);
+}
+
+void UWBFunctionLibrary::AddGameplayTagToActor(AActor* InActor, FGameplayTag TagToAdd)
+{
+	UWBAbilitySystemComponent* ASC = NativeGetWBASCFromActor(InActor);
+
+	if (!ASC->HasMatchingGameplayTag(TagToAdd))
+	{
+		ASC->AddLooseGameplayTag(TagToAdd);
+	}
+}
+
+void UWBFunctionLibrary::RemoveGameplayTagFromActor(AActor* InActor, FGameplayTag TagToRemove)
+{
+	UWBAbilitySystemComponent* ASC = NativeGetWBASCFromActor(InActor);
+
+	if (ASC->HasMatchingGameplayTag(TagToRemove))
+	{
+		ASC->RemoveLooseGameplayTag(TagToRemove);
+	}
+}
+
+void UWBFunctionLibrary::BP_DoesActorHaveTag(AActor* InActor, FGameplayTag TagToCheck, EWBConfirmType& OutConfirmType)
+{
+	OutConfirmType = NativeDoesActorHaveTag(InActor, TagToCheck) ? EWBConfirmType::Yes : EWBConfirmType::No;
+}
---
+++ Source/WarriorBattle/Public/WBFunctionLibrary.h
@@ -0,0 +1,39 @@
+﻿// Fillipe Romero - All Rights Reserved
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameplayTagContainer.h"
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "WBFunctionLibrary.generated.h"
+
+struct FGameplayTag;
+class UWBAbilitySystemComponent;
+
+UENUM()
+enum class EWBConfirmType : uint8
+{
+	Yes,
+	No
+};
+
+UCLASS()
+class WARRIORBATTLE_API UWBFunctionLibrary : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+
+public:
+	static UWBAbilitySystemComponent* NativeGetWBASCFromActor(AActor* InActor);
+
+	static bool NativeDoesActorHaveTag(AActor* InActor, FGameplayTag TagToCheck);
+
+	UFUNCTION(BlueprintCallable, Category="WarriorBattle|FunctionLibrary")
+	static void AddGameplayTagToActor(AActor* InActor, FGameplayTag TagToAdd);
+	
+	UFUNCTION(BlueprintCallable, Category="WarriorBattle|FunctionLibrary")
+	static void RemoveGameplayTagFromActor(AActor* InActor, FGameplayTag TagToRemove);
+
+	UFUNCTION(BlueprintCallable, Category="WarriorBattle|FunctionLibrary", meta=(DisplayName = "Does Actor Have Tag", ExpandEnumAsExecs = "OutConfirmType"))
+	static void BP_DoesActorHaveTag(AActor* InActor, FGameplayTag TagToCheck, EWBConfirmType& OutConfirmType);
+};
+ 
