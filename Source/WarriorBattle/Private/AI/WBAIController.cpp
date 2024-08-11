31efbbc8f57f472d81de2d08a310caa456295629
filliperomero <fillipe.romero@gmail.com>
2024-08-11 00:53:32 -0600

    Add base class for AI Controller

---
+++ Source/WarriorBattle/Private/AI/WBAIController.cpp
@@ -0,0 +1,15 @@
+﻿// Fillipe Romero - All Rights Reserved
+
+#include "AI/WBAIController.h"
+
+#include "WBDebugHelper.h"
+#include "Navigation/CrowdFollowingComponent.h"
+
+AWBAIController::AWBAIController(const FObjectInitializer& ObjectInitializer)
+	: Super(ObjectInitializer.SetDefaultSubobjectClass<UCrowdFollowingComponent>("PathFollowingComponent"))
+{
+	if (UCrowdFollowingComponent* CrowdComp = Cast<UCrowdFollowingComponent>(GetPathFollowingComponent()))
+	{
+		Debug::Print(TEXT("CrowdFollowingComponent valid"), FColor::Green);
+	}
+}
---
+++ Source/WarriorBattle/Public/AI/WBAIController.h
@@ -0,0 +1,18 @@
+﻿// Fillipe Romero - All Rights Reserved
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Runtime/AIModule/Classes/AIController.h"
+#include "WBAIController.generated.h"
+
+UCLASS()
+class WARRIORBATTLE_API AWBAIController : public AAIController
+{
+	GENERATED_BODY()
+
+public:
+	AWBAIController(const FObjectInitializer& ObjectInitializer);
+
+	
+};
