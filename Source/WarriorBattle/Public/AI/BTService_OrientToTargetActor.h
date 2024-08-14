0607bf43b593f0ba62e06d3dd7b24ea360e6618e
filliperomero <fillipe.romero@gmail.com>
2024-08-13 23:18:15 -0600

    Add Custom BT Service to orient enemy towards target

---
+++ Source/WarriorBattle/Public/AI/BTService_OrientToTargetActor.h
@@ -0,0 +1,26 @@
+﻿// Fillipe Romero - All Rights Reserved
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BehaviorTree/BTService.h"
+#include "BTService_OrientToTargetActor.generated.h"
+
+UCLASS()
+class WARRIORBATTLE_API UBTService_OrientToTargetActor : public UBTService
+{
+	GENERATED_BODY()
+
+public:
+	UBTService_OrientToTargetActor();
+
+	virtual void InitializeFromAsset(UBehaviorTree& Asset) override;
+	virtual FString GetStaticDescription() const override;
+	virtual void TickNode(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory, float DeltaSeconds) override;
+
+	UPROPERTY(EditAnywhere, Category = Target)
+	FBlackboardKeySelector InTargetActorKey;
+
+	UPROPERTY(EditAnywhere, Category = Target)
+	float RotationInterpSpeed;
+};
