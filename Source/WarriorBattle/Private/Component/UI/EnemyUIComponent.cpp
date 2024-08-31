f6978878038ae3108f8d09f935882ec43996907d
filliperomero <fillipe.romero@gmail.com>
2024-08-31 00:22:11 -0600

    Add the ability to remove the boss health bar when died

--- Source/WarriorBattle/Private/Component/UI/EnemyUIComponent.cpp
+++ Source/WarriorBattle/Private/Component/UI/EnemyUIComponent.cpp
@@ -1,3 +1,26 @@
 ﻿// Fillipe Romero - All Rights Reserved
 
 #include "Component/UI/EnemyUIComponent.h"
+
+#include "UI/Widget/WBUserWidgetBase.h"
+
+void UEnemyUIComponent::RegisterEnemyDrawnWidget(UWBUserWidgetBase* InWidgetToRegister)
+{
+	EnemyDrawnWidgets.Add(InWidgetToRegister);
+}
+
+bool UEnemyUIComponent::RemoveEnemyDrawnWidget()
+{
+	if (EnemyDrawnWidgets.IsEmpty()) return false;
+
+	for (const auto DrawnWidget : EnemyDrawnWidgets)
+	{
+		if (!IsValid(DrawnWidget)) continue;
+		
+		DrawnWidget->RemoveFromParent();
+	}
+
+	EnemyDrawnWidgets.Empty();
+	
+	return true;
+}
--- Source/WarriorBattle/Public/Component/UI/EnemyUIComponent.h
+++ Source/WarriorBattle/Public/Component/UI/EnemyUIComponent.h
@@ -6,8 +6,21 @@
 #include "PawnUIComponent.h"
 #include "EnemyUIComponent.generated.h"
 
+class UWBUserWidgetBase;
+
 UCLASS(ClassGroup=(Custom), meta=(BlueprintSpawnableComponent))
 class WARRIORBATTLE_API UEnemyUIComponent : public UPawnUIComponent
 {
 	GENERATED_BODY()
+
+public:
+	UFUNCTION(BlueprintCallable)
+	void RegisterEnemyDrawnWidget(UWBUserWidgetBase* InWidgetToRegister);
+	
+	UFUNCTION(BlueprintCallable)
+	bool RemoveEnemyDrawnWidget();
+
+private:
+	UPROPERTY()
+	TArray<UWBUserWidgetBase*> EnemyDrawnWidgets;
 };
