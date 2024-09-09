2ab496836df0cebf8b9c4604c7dfc29781205cea
filliperomero <fillipe.romero@gmail.com>
2024-09-08 22:57:22 -0600

    Add Survival Game Mode and basic functionality to Widget to show countdown

---
+++ Source/WarriorBattle/Private/Game/WBSurvivalGameMode.cpp
@@ -0,0 +1,78 @@
+﻿// Fillipe Romero - All Rights Reserved
+
+#include "Game/WBSurvivalGameMode.h"
+
+void AWBSurvivalGameMode::BeginPlay()
+{
+	Super::BeginPlay();
+
+	checkf(EnemyWaveSpawnerDataTable, TEXT("Forgot to assign a valid Data Table in Survival Game Mode BP"))
+
+	SetCurrentSurvivalGameModeState(EWBSurvivalGameModeState::WaitSpawnNewWave);
+
+	TotalWavesToSpawn = EnemyWaveSpawnerDataTable->GetRowNames().Num();
+}
+
+void AWBSurvivalGameMode::Tick(float DeltaSeconds)
+{
+	Super::Tick(DeltaSeconds);
+
+	if (CurrentSurvivalGameModeState == EWBSurvivalGameModeState::WaitSpawnNewWave)
+	{
+		TimePassedSinceStart += DeltaSeconds;
+
+		if (TimePassedSinceStart >= SpawnNewWaveWaitTime)
+		{
+			TimePassedSinceStart = 0.f;
+
+			SetCurrentSurvivalGameModeState(EWBSurvivalGameModeState::SpawningNewWave);
+		}
+	}
+
+	if (CurrentSurvivalGameModeState == EWBSurvivalGameModeState::SpawningNewWave)
+	{
+		TimePassedSinceStart += DeltaSeconds;
+
+		if (TimePassedSinceStart >= SpawnEnemiesDelayTime)
+		{
+			// TODO: Handle Spawn New Enemies
+
+			TimePassedSinceStart = 0.f;
+
+			SetCurrentSurvivalGameModeState(EWBSurvivalGameModeState::InProgress);
+		}
+	}
+
+	if (CurrentSurvivalGameModeState == EWBSurvivalGameModeState::WaveCompleted)
+	{
+		TimePassedSinceStart += DeltaSeconds;
+
+		if (TimePassedSinceStart >= WaveCompletedWaitTime)
+		{
+			TimePassedSinceStart = 0.f;
+
+			CurrentWaveCount++;
+
+			if (HasFinishedAllWaves())
+			{
+				SetCurrentSurvivalGameModeState(EWBSurvivalGameModeState::AllWavesCompleted);
+			}
+			else
+			{
+				SetCurrentSurvivalGameModeState(EWBSurvivalGameModeState::WaitSpawnNewWave);	
+			}
+		}
+	}
+}
+
+void AWBSurvivalGameMode::SetCurrentSurvivalGameModeState(EWBSurvivalGameModeState NewState)
+{
+	CurrentSurvivalGameModeState = NewState;
+
+	OnSurvivalGameModeStateChangedDelegate.Broadcast(NewState);
+}
+
+bool AWBSurvivalGameMode::HasFinishedAllWaves() const
+{
+	return CurrentWaveCount > TotalWavesToSpawn;
+}
---
+++ Source/WarriorBattle/Public/Game/WBSurvivalGameMode.h
@@ -0,0 +1,90 @@
+﻿// Fillipe Romero - All Rights Reserved
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "WBGameMode.h"
+#include "WBSurvivalGameMode.generated.h"
+
+class AWBEnemyCharacter;
+
+UENUM(BlueprintType)
+enum class EWBSurvivalGameModeState : uint8
+{
+	WaitSpawnNewWave,
+	SpawningNewWave,
+	InProgress,
+	WaveCompleted,
+	AllWavesCompleted,
+	PlayerDied
+};
+
+USTRUCT(BlueprintType)
+struct FWBEnemyWaveSpawnerInfo
+{
+	GENERATED_BODY()
+
+	UPROPERTY(EditAnywhere)
+	TSoftClassPtr<AWBEnemyCharacter> SoftEnemyClassToSpawn;
+	
+	UPROPERTY(EditAnywhere)
+	int32 MinPerSpawnCount = 1;
+	
+	UPROPERTY(EditAnywhere)
+	int32 MaxPerSpawnCount = 3;
+};
+
+USTRUCT(BlueprintType)
+struct FWBEnemyWaveSpawnerTableRow : public FTableRowBase
+{
+	GENERATED_BODY()
+
+	UPROPERTY(EditAnywhere)
+	TArray<FWBEnemyWaveSpawnerInfo> EnemyWaveSpawnerDefinitions;
+	
+	UPROPERTY(EditAnywhere)
+	int32 TotalEnemyToSpawnThisWave = 1;
+};
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnSurvivalGameModeStateChangedSignature, EWBSurvivalGameModeState, CurrentState);
+
+UCLASS()
+class WARRIORBATTLE_API AWBSurvivalGameMode : public AWBGameMode
+{
+	GENERATED_BODY()
+
+protected:
+	virtual void BeginPlay() override;
+	virtual void Tick(float DeltaSeconds) override;
+
+private:
+	void SetCurrentSurvivalGameModeState(EWBSurvivalGameModeState NewState);
+	bool HasFinishedAllWaves() const;
+	
+	UPROPERTY()
+	EWBSurvivalGameModeState CurrentSurvivalGameModeState;
+
+	UPROPERTY(BlueprintAssignable, BlueprintCallable)
+	FOnSurvivalGameModeStateChangedSignature OnSurvivalGameModeStateChangedDelegate;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Wave Definition", meta=(AllowPrivateAccess="true"))
+	TObjectPtr<UDataTable> EnemyWaveSpawnerDataTable;
+
+	UPROPERTY(VisibleDefaultsOnly, BlueprintReadOnly, Category = "Wave Definition", meta=(AllowPrivateAccess="true"))
+	int32 TotalWavesToSpawn;
+
+	UPROPERTY(VisibleDefaultsOnly, BlueprintReadOnly, Category = "Wave Definition", meta=(AllowPrivateAccess="true"))
+	int32 CurrentWaveCount = 1;
+
+	UPROPERTY()
+	float TimePassedSinceStart = 0.f;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Wave Definition", meta=(AllowPrivateAccess="true"))
+	float SpawnNewWaveWaitTime = 5.f;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Wave Definition", meta=(AllowPrivateAccess="true"))
+	float SpawnEnemiesDelayTime = 2.f;
+	
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Wave Definition", meta=(AllowPrivateAccess="true"))
+	float WaveCompletedWaitTime = 5.f;
+};
